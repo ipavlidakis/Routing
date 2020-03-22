5bcd613297cea62b5bb8b5fa1b7e2f7ee02176e6
Ilias Pavidakis <ipavlidakis@gmail.com>
2020-03-22 03:48:05 +0000

    Updates for tvOS

---
+++ Routing-tvOS/Routing_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Routing_tvOS.h
+//  Routing-tvOS
+//
+//  Created by Ilias Pavlidakis on 22/03/2020.
+//  Copyright © 2020 Ilias Pavlidakis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Routing_tvOS.
+FOUNDATION_EXPORT double Routing_tvOSVersionNumber;
+
+//! Project version string for Routing_tvOS.
+FOUNDATION_EXPORT const unsigned char Routing_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Routing_tvOS/PublicHeader.h>
+
+
---
+++ Routing/RoutingFramework.h
@@ -0,0 +1,19 @@
+//
+//  RoutingFramework.h
+//  RoutingFramework
+//
+//  Created by Ilias Pavlidakis on 22/03/2020.
+//  Copyright © 2020 Ilias Pavlidakis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RoutingFramework.
+FOUNDATION_EXPORT double RoutingFrameworkVersionNumber;
+
+//! Project version string for RoutingFramework.
+FOUNDATION_EXPORT const unsigned char RoutingFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RoutingFramework/PublicHeader.h>
+
+
