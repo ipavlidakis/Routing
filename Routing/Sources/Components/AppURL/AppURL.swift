ad70c86f08fd206afc5e5e05402ab99fe9cf2bfa
Ilias Pavidakis <ipavlidakis@gmail.com>
2020-05-04 03:11:56 +0100

    Refactor error to alert

--- Routing/Sources/Components/AppURL/AppURL.swift
+++ Routing/Sources/Components/AppURL/AppURL.swift
@@ -9,27 +9,27 @@
 import Foundation
 
 public struct AppURL: CustomStringConvertible {
-
+    
     public let identifier: String
     public let parameters: [String: Any]
-
+    
     public var description: String { return identifier }
-
+    
     public init(identifier: String, parameters: [String: Any] = [:]) {
         self.identifier = identifier
         self.parameters = parameters
     }
-
+    
     public init?(
         from url: URL,
         appSceme: String?,
         transformQueryItems: Bool = true,
         resolvingAgainstBaseURL: Bool = false) {
-
+        
         guard let components = URLComponents(url: url, resolvingAgainstBaseURL: resolvingAgainstBaseURL) else {
             return nil
         }
-
+        
         guard appSceme != nil, components.scheme == appSceme else {
             if let univesal = AppURL.make(univesalURLComponents: components, transformQueryItems: transformQueryItems, resolvingAgainstBaseURL: resolvingAgainstBaseURL) {
                 self = univesal
@@ -38,19 +38,19 @@ public struct AppURL: CustomStringConvertible {
                 return nil
             }
         }
-
+        
         if let deeplink = AppURL.make(deeplinkURLComponents: components, transformQueryItems: transformQueryItems) {
             self = deeplink
         } else { return nil }
     }
-
+    
     private static func make(
         univesalURLComponents: URLComponents,
         transformQueryItems: Bool,
         resolvingAgainstBaseURL: Bool) -> AppURL? {
-
+        
         guard !univesalURLComponents.path.isEmpty else { return nil }
-
+        
         var parameters: [String: Any] = [:]
         if let queryItems = univesalURLComponents.queryItems {
             parameters = queryItems.reduce(parameters) {
@@ -60,16 +60,16 @@ public struct AppURL: CustomStringConvertible {
                 return dictionary
             }
         }
-
+        
         return AppURL(identifier: univesalURLComponents.path, parameters: parameters)
     }
-
+    
     private static func make(
         deeplinkURLComponents: URLComponents,
         transformQueryItems: Bool) -> AppURL?{
-
+        
         guard let host = deeplinkURLComponents.host, !host.isEmpty else { return nil }
-
+        
         var parameters: [String: Any] = [:]
         if let queryItems = deeplinkURLComponents.queryItems {
             parameters = queryItems.reduce(parameters) {
@@ -79,58 +79,59 @@ public struct AppURL: CustomStringConvertible {
                 return dictionary
             }
         }
-
+        
         return AppURL(identifier: host, parameters: parameters)
     }
 }
 
 extension AppURL {
-
+    
     public static let templateParameterKey = "appURLErrorTemplateParameter"
-    public static let errorIdentifier = "appURLError"
-    public static func makeError(parameters: [String: Any]) -> AppURL {
-        AppURL(identifier: errorIdentifier, parameters: parameters)
-    }
-
+    public static let alertIdentifier = "appAlert"
+    
     public static func makeAlert(
         title: String? = nil,
         message: String? = nil,
         actions: [ActionTemplate] = []
     ) -> AppURL {
-        AppURL.makeError(parameters: [
-            AppURL.templateParameterKey: AppURL.ErrorTemplate(
-                title: title,
-                message: message,
-                actions: actions
-            )
+        AppURL(
+            identifier: alertIdentifier,
+            parameters: [
+                AppURL.templateParameterKey: AppURL.AlertTemplate(
+                    title: title,
+                    message: message,
+                    actions: actions
+                )
         ])
     }
-
+    
     public struct ActionTemplate {
         public let name: String
         public let isCancel: Bool
         public let isDestructive: Bool
         public let action: (() -> Void)?
-
-        public init(name: String,
-                    isCancel: Bool,
-                    isDestructive: Bool,
-                    action: (() -> Void)?) {
+        
+        public init(
+            name: String,
+            isCancel: Bool,
+            isDestructive: Bool,
+            action: (() -> Void)?) {
             self.name = name
             self.isCancel = isCancel
             self.isDestructive = isDestructive
             self.action = action
         }
     }
-
-    public struct ErrorTemplate {
+    
+    public struct AlertTemplate {
         public let title: String?
         public let message: String?
         public let actions: [ActionTemplate]
-
-        public init(title: String?,
-                    message: String?,
-                    actions: [ActionTemplate]) {
+        
+        public init(
+            title: String?,
+            message: String?,
+            actions: [ActionTemplate]) {
             self.title = title
             self.message = message
             self.actions = actions
